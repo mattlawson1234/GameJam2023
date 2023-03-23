1b734114213e524d7a4e47e8abce0dfd9555f107
tjl-47 <tjlouis47@gmail.com>
2023-03-22 18:00:57 -0700

    Removed the debugging code

--- GameJam/src/MazeGame.java
+++ GameJam/src/MazeGame.java
@@ -284,21 +284,6 @@ public class MazeGame {
 
             }
 
-			// Print the maze
-			for(int i = 0; i < 20; i++){
-
-				for(int j = 0; j < 20; j++){
-	
-					System.out.print(maze[i][j] + "\t");
-	
-				}
-	
-				System.out.println();
-	
-			}
-
-			System.out.println();
-
         }
 
         // Generate the exit tile
